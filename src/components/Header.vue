024edf0f58da0db81c50d7e7b45bfd93316bda5f
Riley Gatto <rmgatto64@gmail.com>
2025-09-12 14:14:08 +1000

    Initial commit

---
+++ src/components/Header.vue
@@ -0,0 +1,12 @@
+<template>
+    <header>
+        <h1>Riley Gatto</h1>
+    </header>
+</template>
+
+<style scoped>
+    header {
+        width: 100%;
+        border-bottom: 4px solid #fefefe;
+    }
+</style>
